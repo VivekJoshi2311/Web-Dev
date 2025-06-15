4eafbc872bd14604e0db6da73f2f822f2fdf8164
VivekJoshi2311 <2021wb86568@wilp.bits-pilani.ac.in>
2025-06-15 12:28:18 +0530

    Update HelloJava.java

--- HelloJava.java
+++ HelloJava.java
@@ -1,2 +1,2 @@
-public class HelloJava { public static void main(String[] args) { System.out.println("Hello Java"); } }
+public class HelloJava { public static void main(String[] args) { logger.info("Hello Java"); } }
 // Version 2 Update
